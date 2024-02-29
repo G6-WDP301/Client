852abb45aea1178e26664436bfb9e6a9fdbea31f
QuanHH <huyquanwork95@gmail.com>
2024-02-29 19:55:05 +0700

    update profile

--- src/layout/NavbarLogin/index.jsx
+++ src/layout/NavbarLogin/index.jsx
@@ -131,9 +131,9 @@ export default function index() {
               {/* Dropdown menu */}
               {showMenu && (
                 <div className="flex dropdown-menu absolute top-full right-0 mt-2 bg-white border border-gray-300 rounded-md shadow-md overflow-hidden transform origin-top transition-all duration-300" style={{ border: '1px solid grey', boxShadow: '2px 4px 4px rgba(0, 0, 0, 0.4)' }}>
-                 <ul className="py-3 px-5">
+                  <ul className="py-3 px-5">
                     <div className='rounded-full bg-orange-400 text-slate-50 px-3 mb-3 hover:bg-orange-500 hover:cursor-pointer'
-                       style={{boxShadow:'2px 4px 4px rgba(0, 0, 0, 0.3)'}}
+                      style={{ boxShadow: '2px 4px 4px rgba(0, 0, 0, 0.3)' }}
                     >
                       <span className='flex justify-center text-center font-medium'>User</span>
                     </div>
